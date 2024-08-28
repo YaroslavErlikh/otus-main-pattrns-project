5f74d4bfdf521de027781e3624624808cecf0f27
Ярослав Эрлих <i.erlikh@rt-solar.ru>
2024-08-28 22:15:32 +0500

    comment unused methods

--- editor/service/src/main/groovy/erlikh/yaroslav/editor/mapper/SmartLinkRuleConverter.groovy
+++ editor/service/src/main/groovy/erlikh/yaroslav/editor/mapper/SmartLinkRuleConverter.groovy
@@ -5,18 +5,18 @@ import erlikh.yaroslav.editor.dto.SmartLinkRuleUpdateDto
 import erlikh.yaroslav.editor.entity.SmartLinkRule
 
 class SmartLinkRuleConverter {
-
-    static SmartLinkRuleDto toDto(String name, String type, String rule, String link) {
-        new SmartLinkRuleDto(name, type, rule, link)
-    }
+//
+//    static SmartLinkRuleDto toDto(String name, String type, String rule, String link) {
+//        new SmartLinkRuleDto(name, type, rule, link)
+//    }
 
     static SmartLinkRuleDto toDto(SmartLinkRule smartLinkRule) {
         new SmartLinkRuleDto(smartLinkRule.name, smartLinkRule.type, smartLinkRule.rule, smartLinkRule.link)
     }
 
-    static SmartLinkRule toEntity(String name, String type, String rule, String link) {
-        new SmartLinkRule(name, type, rule, link)
-    }
+//    static SmartLinkRule toEntity(String name, String type, String rule, String link) {
+//        new SmartLinkRule(name, type, rule, link)
+//    }
 
     static SmartLinkRule toEntity(SmartLinkRuleUpdateDto dto) {
         new SmartLinkRule(dto.name, dto.type, dto.rule, dto.link)
